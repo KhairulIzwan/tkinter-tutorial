5970ac6b704aae389bba2833941ff4ae70460f8a
Khairul <wansnap@gmail.com>
2019-05-11 23:27:31 +0800

    New commit

---
+++ intro_1.py
@@ -0,0 +1,22 @@
+'''
+Basic of Tkinter -- Intro
+'''
+try:
+    # Python 2
+    from Tkinter import *
+
+except ImportError:
+    # Python 3
+    from tkinter import *
+
+
+class Window(Frame):
+
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
+        self.master = master
+
+
+root = Tk()
+app = Window(root)
+root.mainloop()
